ddeaadb62b60f311d79d786eebff2954985b5e55
ChulJJA <fpsemfaos@gmail.com>
2020-03-10 16:29:24 +0900

    sOMETHINGS WRONG

--- Engine/Component_Player.h
+++ Engine/Component_Player.h
@@ -30,7 +30,7 @@ public:
 	
     void Init(Object* obj) override;
     void Update(float dt) override;
-    void Attack();
+    void SetHPBar();
     int Get_Damage()
     {
         return damage;
@@ -59,6 +59,7 @@ public:
     {
 		curr_state = state;
     }
+    void PlayerMove(float max_velocity, float min_velocity);
 private:
     Object* hp_bar = nullptr;
     Item::Item_Kind belong_item = Item::Item_Kind::None;
@@ -67,4 +68,5 @@ private:
     float regeneration_timer = 0.f;
     float bulkup_timer = 0.f;
 	Char_State curr_state;
+    vector2 velocity{};
 };
