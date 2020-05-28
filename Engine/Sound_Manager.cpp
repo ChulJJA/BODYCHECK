be46fadef45f0b16f4b14b0354f249d7fa211438
ChulJJA <fpsemfaos@gmail.com>
2020-05-28 14:22:46 +0900

    Added Wall Crack sound

--- Engine/Sound_Manager.cpp
+++ Engine/Sound_Manager.cpp
@@ -96,12 +96,14 @@ void Sound::LoadSound()
 	result = FMOD_System_CreateSound(f_system, "Sounds/MissilePrepare.wav", FMOD_DEFAULT, nullptr, &sound[static_cast<int>(SOUND::MissilePrepare)]);
 	ErrorCheck(result);
 
+	result = FMOD_System_CreateSound(f_system, "Sounds/WallCrack.wav", FMOD_DEFAULT, nullptr, &sound[static_cast<int>(SOUND::WallCrack)]);
+	ErrorCheck(result);
+	
 	result = FMOD_System_CreateSoundGroup(f_system, "BGM", &bgm_group);
 	ErrorCheck(result);
 
 	result = FMOD_System_CreateSoundGroup(f_system, "SFX", &sfx_group);
 	ErrorCheck(result);
-
 }
 
 void Sound::UnLoad()
